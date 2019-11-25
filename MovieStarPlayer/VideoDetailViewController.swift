26dbeeda0c391e15d9f60b220ed0bf1f5207bd85
sangbeomLee <wing951@naver.com>
2019-11-25 20:44:43 +0900

    화면 넘어가기
    main화면에서 Player화면으로 넘어가는것 구현

---
+++ MovieStarPlayer/VideoDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VideoDetailViewController.swift
+//  MovieStarPlayer
+//
+//  Created by 이상범 on 2019/11/25.
+//  Copyright © 2019 sangbeom Lee. All rights reserved.
+//
+
+import UIKit
+
+class VideoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MovieStarPlayer/ViewController.swift
+++ MovieStarPlayer/ViewController.swift
@@ -15,8 +15,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func addVideo(_ sender: Any) {
+    @IBAction func moveToAddVideo(_ sender: Any) {
+        
+        // UIStoryboard 로 움직이고 싶을 때 사용한다.
+        let playerVC = UIStoryboard(name: "Player", bundle: nil).instantiateViewController(withIdentifier: "navigationPlayer")
+        
+        self.present(playerVC, animated: true, completion: nil)
+        
     }
+    
     
 }
 
