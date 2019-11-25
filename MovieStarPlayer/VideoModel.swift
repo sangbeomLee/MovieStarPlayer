02143f9ed3c6a1e5f19e46e1a482757aaf88f67a
sangbeomLee <wing951@naver.com>
2019-11-25 20:24:53 +0900

    Create VideoModel

---
+++ MovieStarPlayer/VideoModel.swift
@@ -0,0 +1,25 @@
+//
+//  VideoModel.swift
+//  MovieStarPlayer
+//
+//  Created by 이상범 on 2019/11/25.
+//  Copyright © 2019 sangbeom Lee. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+class VideoModel {
+
+    var title: String
+    var video: AVPlayer
+    var videoIMG: UIImage?
+    var rating: Int
+    
+    init(title: String, video: AVPlayer, videoIMG: UIImage?, rating: Int){
+        self.title = title
+        self.video = video
+        self.videoIMG = videoIMG
+        self.rating = rating
+    }
+}
