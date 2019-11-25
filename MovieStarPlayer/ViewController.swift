bba66a503fb9f95ea18d79d15a70e0c10f8d73cc
sangbeomLee <wing951@naver.com>
2019-11-25 17:36:27 +0900

    Initial Commit

---
+++ MovieStarPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieStarPlayer
+//
+//  Created by 이상범 on 2019/11/25.
+//  Copyright © 2019 sangbeom Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
